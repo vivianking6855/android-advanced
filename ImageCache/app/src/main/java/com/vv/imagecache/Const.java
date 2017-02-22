5cdbe1d1ddc69f8d18d2286e367c3b423da1f7b6
vivian_sun <vivian_sun@asus.com>
2017-02-22 15:09:51 +0800

    init image cache

---
+++ ImageCache/app/src/main/java/com/vv/imagecache/Const.java
@@ -0,0 +1,12 @@
+package com.vv.imagecache;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by vivian on 2017/2/22.
+ */
+
+public final class Const {
+    public final static DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+}
--- ImageCache/app/src/main/java/com/vv/imagecache/MainActivity.java
+++ ImageCache/app/src/main/java/com/vv/imagecache/MainActivity.java
@@ -1,13 +1,30 @@
 package com.vv.imagecache;
 
+import android.graphics.BitmapFactory;
+import android.support.v4.util.LruCache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mTVShow;
+
+    private ImageResizer mImageResizer = new ImageResizer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+    private void initView() {
+        mTVShow = (TextView) findViewById(R.id.show);
+        mTVShow.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+    }
+
 }
