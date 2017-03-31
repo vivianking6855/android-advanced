4871122763c27580288b82f7be017d89e085f619
vivian_sun <vivian_sun@asus.com>
2017-03-31 10:42:28 +0800

    update performance

--- PerformanceDemo/app/src/main/java/com/vv/performancedemo/HVOptimizeActivity.java
+++ PerformanceDemo/app/src/main/java/com/vv/performancedemo/HVOptimizeActivity.java
@@ -1,13 +1,45 @@
 package com.vv.performancedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.ImageView;
+
+import com.vv.performancedemo.common.ImageUtils;
 
 public class HVOptimizeActivity extends AppCompatActivity {
+
+    private View offlineView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hvoptimize);
+
+        initView();
     }
+
+    private void initView() {
+        ImageView avatar1 = (ImageView) findViewById(R.id.avatar1);
+        ImageView avatar2 = (ImageView) findViewById(R.id.avatar2);
+        avatar1.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(getResources(), R.drawable.card1, 100, 100));
+        avatar2.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(getResources(), R.drawable.card2, 100, 100));
+    }
+
+    public void showOffline(View v) {
+        if (offlineView == null) {
+            ViewStub noDataViewStub = (ViewStub) findViewById(R.id.offline_view);
+            offlineView = noDataViewStub.inflate();
+        } else {
+            offlineView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideOffline(View v) {
+        if (offlineView != null) {
+            offlineView.setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
--- PerformanceDemo/app/src/main/java/com/vv/performancedemo/MainActivity.java
+++ PerformanceDemo/app/src/main/java/com/vv/performancedemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.vv.performancedemo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
