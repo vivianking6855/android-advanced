6c88357c914981b395ebf51d593340637247ed48
vivian_sun <vivian_sun@asus.com>
2017-03-31 19:18:35 +0800

    add readme

--- AppUniform/app/src/main/java/com/vv/appuniform/MainActivity.java
+++ AppUniform/app/src/main/java/com/vv/appuniform/MainActivity.java
@@ -1,14 +1,37 @@
 package com.vv.appuniform;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.vv.applib.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void loadData() {
+
     }
 
 
