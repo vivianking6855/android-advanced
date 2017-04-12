687129745ae9381143de5ba02875758cea4b5f9c
vivian_sun <vivian_sun@asus.com>
2017-04-12 14:16:34 +0800

    fix RxAndroid

--- WebDemo/app/src/main/java/com/open/webdemo/activity/MainActivity.java
+++ WebDemo/app/src/main/java/com/open/webdemo/activity/MainActivity.java
@@ -8,10 +8,10 @@ import android.widget.TextView;
 
 import com.open.webdemo.R;
 import com.open.webdemo.engine.WebsiteEngine;
-import com.open.webdemo.entity.UrlConfig;
 
 import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTVShow;
 
     // website
-    private CompositeDisposable compositeDisposable;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +33,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void okhttpConfigClick(View view) {
-        Disposable config = (Observable.fromCallable(() -> WebsiteEngine.getInstance().getConfig()).subscribeOn(Schedulers.io()))
+        Disposable config = Observable.fromCallable(() -> WebsiteEngine.getInstance().getConfig())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(urlConfig -> {
                             final String result = WebsiteEngine.getGson().toJson(urlConfig.Debug).toString();
                             Log.d(TAG, "" + result);
+                            mTVShow.setText(result);
                         },
                         error -> Log.w(TAG, "okhttpConfigClick ex:", error),
                         () -> Log.d(TAG, "okhttpConfigClick complete"));
@@ -45,10 +48,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void okhttpSearchClick(View view) {
         final String search_key = "zenfone3";
-        Disposable search = Single.fromCallable(() -> WebsiteEngine.getInstance().search(search_key)).subscribeOn(Schedulers.io())
+        Disposable search = Single.fromCallable(() -> WebsiteEngine.getInstance().search(search_key))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(list -> {
                             final String result = WebsiteEngine.getGson().toJson(list).toString();
                             Log.d(TAG, "" + result);
+                            mTVShow.setText(result);
                         },
                         error -> Log.w(TAG, "okhttpSearchClick ex:", error));
         compositeDisposable.add(search);
