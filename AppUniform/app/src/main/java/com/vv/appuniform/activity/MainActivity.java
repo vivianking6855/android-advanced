ff72f3d6f2937e1a33d1d211bc144929e4653bad
vivian_sun <vivian_sun@asus.com>
2017-09-20 15:14:08 +0800

    update app uniform

---
+++ AppUniform/app/src/main/java/com/vv/appuniform/activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.vv.appuniform.activity;
+
+import android.os.Bundle;
+
+import com.vv.appuniform.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
