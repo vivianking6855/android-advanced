6cab513faeb5613f09739c85ba68607ab10dc5fa
vivian_sun <vivian_sun@asus.com>
2017-11-13 10:49:46 +0800

    cache demo create

---
+++ CacheDemo/app/src/main/java/com/vv/cache/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package com.vv.cache.activity;
+
+import android.os.Bundle;
+
+import com.vv.cache.R;
+import com.vv.cache.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
